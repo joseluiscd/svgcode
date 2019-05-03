dde695629c2b5dcd08c86820d47f03ddeb1f923d
José Luis C.D <jlcd0003@red.ujaen.es>
2019-05-03 22:58:56 +0200

    Implemented transform node in SVG read

--- src/input/svg.rs
+++ src/input/svg.rs
@@ -5,7 +5,7 @@ use lyon_path::Path;
 use lyon_path::math::{Point, point};
 use std::path::Path as FilePath;
 use std::io::{Error, ErrorKind};
-use usvg::{Paint, PathSegment};
+use usvg::{Paint, PathSegment, NodeExt};
 
 pub const FALLBACK_COLOR: Color = Color {
     red: 0,
@@ -26,7 +26,7 @@ impl <'a> SvgParser<'a> {
 }
 
 impl <'a> Parser for SvgParser<'a> {
-    fn parse(mut self) -> Result<Drawing, Error> {
+    fn parse(self) -> Result<Drawing, Error> {
         let opt = usvg::Options::default();
 
         let tree = usvg::Tree::from_file(&self.input, &opt).or_else(|e|{
@@ -37,22 +37,29 @@ impl <'a> Parser for SvgParser<'a> {
 
         for node in tree.root().descendants() {
             if let usvg::NodeKind::Path(ref p) = *node.borrow() {
+                let transform = node.transform();
 
                 let mut builder = Path::builder();
 
                 for ref segment in &p.segments {
                     match segment{
                         PathSegment::MoveTo{x, y} => {
-                            builder.move_to(point(*x as f32, *y as f32));
+                            let (x, y) = transform.apply(*x, *y);
+                            builder.move_to(point(x as f32, y as f32));
                         },
                         PathSegment::LineTo{x, y} => {
-                            builder.line_to(point(*x as f32, *y as f32));
+                            let (x, y) = transform.apply(*x, *y);
+                            builder.line_to(point(x as f32, y as f32));
                         },
                         PathSegment::CurveTo { x1, y1, x2, y2, x, y } => {
+                            let (x, y) = transform.apply(*x, *y);
+                            let (x1, y1) = transform.apply(*x1, *y1);
+                            let (x2, y2) = transform.apply(*x2, *y2);
+
                             builder.cubic_bezier_to(
-                                point(*x1 as f32, *y1 as f32),
-                                point(*x2 as f32, *y2 as f32),
-                                point(*x as f32, *y as f32)
+                                point(x1 as f32, y1 as f32),
+                                point(x2 as f32, y2 as f32),
+                                point(x as f32, y as f32)
                             )
                         },
                         PathSegment::ClosePath => {
